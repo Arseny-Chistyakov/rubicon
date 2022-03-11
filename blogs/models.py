f928b9d388b7840545a6c514ff990442dc4a67f8
ArsenyChistyakov <samson200289@mail.ru>
2022-03-11 19:24:55 +0300

    fix small bags

--- blogs/models.py
+++ blogs/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class New(models.Model):
     header = models.CharField(max_length=256)
-    image = models.ImageField(upload_to='')
+    image = models.ImageField(upload_to='blogs_images')
     body = models.CharField(max_length=256)
     date_public = models.DateField(auto_now=True)
 
