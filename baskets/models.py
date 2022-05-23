afe53654ed4fc3c089c7b51c8a9c65e41eb75219
ArsenyChistyakov <samson200289@mail.ru>
2022-05-24 02:44:33 +0300

    added logic in quantity(products-basket-orders) 1 method as_manager()

--- baskets/models.py
+++ baskets/models.py
@@ -4,7 +4,17 @@ from products.models import Product
 from users.models import User
 
 
+class BasketQuerySet(models.QuerySet):
+    def delete(self, *args, **kwargs):
+        for item in self:
+            item.product.quantity += item.quantiy
+            item.product.save()
+        super(BasketQuerySet, self).delete(*args, **kwargs)
+
+
 class Basket(models.Model):
+    object = BasketQuerySet.as_manager()
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
@@ -27,3 +37,21 @@ class Basket(models.Model):
     def total_sum(self):
         baskets = Basket.objects.filter(user=self.user)
         return sum(basket.sum() for basket in baskets)
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            item = self.get_item(int(self.pk))
+            self.product.quantity -= self.quantity - item
+        else:
+            self.product.quantity -= self.quantity
+        self.product.save()
+        super(Basket, self).save(*args, **kwargs)
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.get(pk=pk).quantity
+
+    def delete(self, *args, **kwargs):
+        self.product.quantity += self.quantity
+        self.save()
+        super(Basket, self).delete(*args, **kwargs)
--- orders/views.py
+++ orders/views.py
@@ -38,7 +38,7 @@ class OrderCreate(CreateView):
                     form.initial['product'] = basket_item[num].product
                     form.initial['quantity'] = basket_item[num].quantity
                     form.initial['price'] = basket_item[num].product.price
-                # basket_item.delete()
+                basket_item.delete()
             else:
                 formset = OrderFormSet()
 
